bb83105706d9ef57b810a4fb8cb18447581f7eda
Kian Jafari <kianjafari660@gmail.com>
2026-04-19 12:31:59 +0330

    :goal_net:  Fixed errors related to project mapping for migration assembly refrence

--- backend/ChatApp.API/Program.cs
+++ backend/ChatApp.API/Program.cs
@@ -8,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("ChatApp.API")));
 
 var app = builder.Build();
 
